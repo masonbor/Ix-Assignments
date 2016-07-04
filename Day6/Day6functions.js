5db78425e86b036f29ab263788193aced43e7472
Mason Borden <masonbor@umich.edu>
2016-07-04 15:40:48 +0200

    day 6 folder

--- Day6/Day6functions.js
+++ Day6/Day6functions.js
@@ -10,16 +10,16 @@ function average(numbers_list) {
 function assignGrade(grade) {
     if (grade < 60) {
         return 'F';
-    } else if(grade >== 60 && grade < 70) {
+    } else if(grade >= 60 && grade < 70) {
         return 'D';
     }
-    else if(grade >== 70 && grade < 80) {
+    else if(grade >= 70 && grade < 80) {
         return 'C';
     }
-    else if(grade >== 80 && grade < 90) {
+    else if(grade >= 80 && grade < 90) {
         return 'B';
     }
-    else if(grade >== 90) {
+    else {
         return 'A';
     }
 }
@@ -34,45 +34,60 @@ function pluralize(num, noun) {
 function longestWord(sentence) {
     var splitSentence = sentence.split(" ");
     var currLongestWord = " ";
-    var currLongestLength;
-    for (var i = 0; i > splitSentence.length; i--) {
-        var tempLength = splitSentence[i+1].length;
-        if(tempLength > currLongestLength) {
-            tempLength = currLongestLength;
-            currLongestWord = splitSentence[i+1];
+    for (var i = 0; i < splitSentence.length; i++) {
+        if (splitSentence[i].length > currLongestWord.length) { 
+            currLongestWord = splitSentence[i];
         }
     }
     return currLongestWord;
 }
 
-function palindrome(word) {
-    // if (palindrome)
-    //     return 'yes'
-    // else 
-    //     return 'no'
+function palindrome(word) { 
+    var backward = "";
+    for (var i = word.length - 1; i >= 0; i--) {
+        backward += word[i]; 
+    };
+
+    if (word === backward) {
+        return "yes";
+    }
+    else { 
+        return "no"
+    }
 }
 
 function letterCounter(phrase, letter) {
-    var currCount;
+    var currCount = 0;
     for(var i = 0; i < phrase.length; i++) {
-        if(phrase[i-1] !== letter)
+        if(phrase[i] === letter)
             currCount++;
     }
-    return phrase.length;
+    return currCount;
 }
 
 function longestPalindrome(sentence) {
-    // if (longest word in sentence is a palindrome) {
-    //     return longest + " is a palindrome";
-    // }
-    // else {
-    //     return longest + " is not a palindrome";
-    // }
+    var longest = longestWord(sentence);
+    if (palindrome(longest) === "yes") { 
+        return longest + " is a palindrome";
+    }
+
+    else { 
+        return longest + " is not a palindrome";
+    }
+
 }
 
 function areAnagrams (sentence1, sentence2) {
-    // if (sentence1 and sentence2 are anagrams)
-    //     return 'yes'
-    // else
-    //     return 'no'
+
+    var newSentence1 = sentence1.split("").sort().join().trim();
+    var newSentence2 = sentence2.split("").sort().join().trim();
+
+    if (newSentence1 ===  newSentence2) { 
+        return 'yes';
+
+    }
+    else { 
+        return 'no';
+
+    }
 }
