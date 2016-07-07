b0ed5d20ea0e4d5c52c4a02fefb211ac5a6e31da
Mason Borden <masonbor@umich.edu>
2016-07-07 15:52:12 +0200

    Day 9 morning submission

--- Day8/grocerylist.js
+++ Day8/grocerylist.js
@@ -7,7 +7,7 @@ app.controller('GroceryCtrl', function($scope) {
 		var isInList = false;
 		for (var k = 0; k < $scope.list.length; k++){ 
 
-			if ($scope.item === $scope.list[k].name) {
+			if ($scope.item.toLowerCase() === $scope.list[k].name) {
 
 				$scope.list[k].amount += parseInt($scope.quantity);
 				isInList = true;
@@ -15,10 +15,26 @@ app.controller('GroceryCtrl', function($scope) {
 
 
 		}
-		if (isInList === false){	
+		if (isInList === false) {	
+			if (isNaN($scope.item) === false) { 
+				$scope.item = "";
+				$scope.quantity = "";
+				return;
+
+			}
+
+			if (isNaN($scope.quantity) === true) { 
+				$scope.item = "";
+				$scope.quantity = "";
+				return;
+
+			}
+
+
 			var newGrocery = { 
-				name: $scope.item, 
-				amount: parseInt($scope.quantity)
+				name: $scope.item.toLowerCase(), 
+				amount: parseInt($scope.quantity),
+				isEditing: false
 			};
 			$scope.list.push(newGrocery);
 			
@@ -26,20 +42,33 @@ app.controller('GroceryCtrl', function($scope) {
 
 		$scope.item = "";
 		$scope.quantity= "";
-	
-
 	};
 
 	$scope.addAmount = function(i) { 
 		i.amount++;
-
-
 	};
 
-	$scope.subtractAmount = function(i) { 
+	$scope.subtractAmount = function(i, index) { 
 		if (i.amount > 0) {
 			i.amount--;
 		}
+
+		else { 
+			$scope.list.splice(index, 1);
+
+		}
+	};
+
+	$scope.deleteItem = function (index) { 
+			$scope.list.splice(index, 1);
+
+	};
+
+	$scope.emptyCart = function () { 
+			$scope.list.length = 0;
+
+			
+
 	};
 
 
